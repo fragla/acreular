8fca218c61b0eb41b86351822245c284b1e198ca
Fraser Morton <fraser.morton@glasgow.ac.uk>
2020-01-27 21:04:40 +0000

    Adding shiny_acreular function to launch shiny app from the command line.

---
+++ R/shiny.R
@@ -0,0 +1,23 @@
+#' Launch shiny acreular interface
+#'
+#' \code{shiny_acreular} launches a shiny interface for browser based ACR/EULAR calculations.
+#'
+#' @param display.mode The display mode to be passed to \link[shiny]{runApp}
+#' @return NULL
+#' @examples
+#' \donttest{
+#' shiny_acreular()
+#' shiny_acreular(display.mode="normal")
+#' }
+#' @export
+shiny_acreular <- function(display.mode = "normal") {
+  pkgs <- c("shiny", "DT", "mime", "readxl", "shinycssloaders")
+  missing <- sapply(pkgs, function(x){!requireNamespace(x, quietly=TRUE)})
+  if (any(missing)) {
+    stop(paste("The following package(s) are required for shiny_acreular to work:",
+               paste(pkgs[missing], collapse=", ")),
+         call. = FALSE)
+  }
+  app_dir <- system.file("shiny", package = "acreular")
+  shiny::runApp(app_dir, display.mode = display.mode)
+}
