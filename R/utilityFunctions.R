92041392c30f8a550620736168e2a72f22a3309e
Fraser Morton <fraser.morton@glasgow.ac.uk>
2020-02-18 13:22:55 +0000

    Exporting is.Date function and added custom is.empty function.

--- R/utilityFunctions.R
+++ R/utilityFunctions.R
@@ -1,4 +1,39 @@
+#' Checks if object is of class Date.
+#'
+#'
+#' @param date
+#'
+#' @examples
+#' date <- as.Date("2010-01-01")
+#'
+#' is.Date(date)
+#'
+#' @export
 is.Date <- function(x) inherits(x, 'Date')
+
+is.empty <- function(x) {
+  if(length(x)==0) {
+    return(TRUE)
+  }
+
+  if(is.na(x)) {
+    return(TRUE)
+  }
+
+  if(is.null(x)) {
+    return(TRUE)
+  }
+
+  if(is.nan(x)) {
+    return(TRUE)
+  }
+
+  if (is.character(x) && nchar(gsub("[[:space:]]", "", x))==0) {
+    return(TRUE)
+  }
+
+  return(FALSE)
+}
 
 #' Convert two dates to a duration in days.
 #'
